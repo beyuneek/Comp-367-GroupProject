d13ce63caee58e0849ea5b4c34636ec95230bcff
Parth Sharma <113319598+beyuneek@users.noreply.github.com>
2024-04-17 22:14:35 -0400

    Update ParthAssignment1Comp303Sec001ApplicationTests.java.java

--- src/test/java/com/example/demo/ParthAssignment1Comp303Sec001ApplicationTests.java.java
+++ src/test/java/com/example/demo/ParthAssignment1Comp303Sec001ApplicationTests.java.java
@@ -1,1 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParthAssignment1Comp303Sec001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
 
