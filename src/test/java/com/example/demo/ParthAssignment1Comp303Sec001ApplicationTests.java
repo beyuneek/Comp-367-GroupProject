431aaee00eeeafbd5b9bc7fdb793fc3433e1c519
Parth Sharma <113319598+beyuneek@users.noreply.github.com>
2024-04-17 22:33:29 -0400

    Update ParthAssignment1Comp303Sec001ApplicationTests.java

--- src/test/java/com/example/demo/ParthAssignment1Comp303Sec001ApplicationTests.java
+++ src/test/java/com/example/demo/ParthAssignment1Comp303Sec001ApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.parth.mvc.SpringMvcLoginApplication;
 
 @SpringBootTest(classes = SpringMvcLoginApplication.class)
 public class ParthAssignment1Comp303Sec001ApplicationTests {
